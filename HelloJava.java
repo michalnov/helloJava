7cbc664de4fd72b163f4e9c5f1d87856aaf4842a
michalnov <32653270+michalnov@users.noreply.github.com>
2018-12-10 12:19:08 +0100

    Add files via upload

---
+++ HelloJava.java
@@ -0,0 +1,57 @@
+public class HelloJava {
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 40;
+        int c = 5;
+
+
+
+//        if (a < 0 || b < 0 || c < 0){
+//            System.out.println("wrong input");
+//            return;
+//        }
+//
+//        if ((a+b)< c){
+//            System.out.println("wrong");
+//            return;
+//        }
+//        if ((c+b)< a){
+//            System.out.println("wrong");
+//            return;
+//        }
+//        if ((a+c)< b){
+//            System.out.println("wrong");
+//            return;
+//        }
+//        System.out.println("it is possible to create triange");
+
+
+
+
+//        if ((a+b)>c){
+//            if ((b+c)>a){
+//                if ((c+a)>b){
+//                    System.out.println("it is possible to create triange");
+//                    return;
+//                }
+//            }
+//        }
+//        System.out.println("wrong");
+
+
+
+
+        
+
+//        if ((a + b) > c && (b + c) > a && (c + a) > b){
+//            System.out.println("it is possible to create triange");
+//        } else {
+//            System.out.println("wrong");
+//        }
+
+
+
+
+    }
+}
