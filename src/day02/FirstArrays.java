e0d0879878236c65d388457560c6ccf92b6e6385
michalnov <32653270+michalnov@users.noreply.github.com>
2018-12-11 13:09:15 +0100

    Update FirstArrays.java

--- src/day02/FirstArrays.java
+++ src/day02/FirstArrays.java
@@ -4,5 +4,28 @@ public class FirstArrays {
 
     public static void main(String[] args) {
 
+        int[] pole = {15,25,3,4,5,6,4,7,8};
+        int sucet = 0;
+
+        System.out.println("size of array is :" + pole.length);
+
+        for (int i = 0; i < pole.length; i++) {
+            //System.out.println(pole[i]);
+            sucet = sucet + pole[i];
+        }
+
+        System.out.println("sucet 1 :"+sucet);
+        System.out.println("priemer 1 :" + (double)sucet / pole.length);
+        sucet = 0;
+
+        for (int i = 0; i < pole.length; i++) {
+            System.out.println(pole[i]);
+            sucet = sucet + pole[i];
+        }
+        System.out.println("sucet 2 :"+sucet);
+
+        double priemer = (double) sucet / pole.length;
+
+        System.out.println("priemer 2:" + priemer);
     }
 }
