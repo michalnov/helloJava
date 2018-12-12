290466974fc7cd736c90ab9e8dffef9f644a6730
michalnov <32653270+michalnov@users.noreply.github.com>
2018-12-12 10:01:49 +0100

    Update FirstArrays_2D.java

--- src/day03/FirstArrays_2D.java
+++ src/day03/FirstArrays_2D.java
@@ -3,6 +3,28 @@ package day03;
 public class FirstArrays_2D {
 
     public static void main(String[] args) {
+        int[][] pole2D = new int[3][8];
 
+        Random random = new Random();
+
+        for (int i = 0; i < pole2D.length; i++) {
+
+            for (int j = 0; j < pole2D[0].length; j++) {
+                pole2D[i][j] = random.nextInt(50)+100;      // generuje od 100 do 100 + 50
+                System.out.print(" "+pole2D[i][j]);               // a = 100 , b = 150  bound = b - a = 50 , posun = a
+            }
+            System.out.println();
+        }
+
+        System.out.println("nex");
+
+        for (int[] riadky : pole2D){
+
+            for (int element : riadky){
+                element = random.nextInt(50)+100;      // generuje od 100 do 100 + 50
+                System.out.print(" "+element);
+            }
+            System.out.println();
+        }
     }
 }
