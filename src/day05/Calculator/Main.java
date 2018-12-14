a0455f977b4871fb1e4e62c4a26e645ae3421a73
michalnov <32653270+michalnov@users.noreply.github.com>
2018-12-14 13:24:23 +0100

    Add files via upload

--- src/day05/Calculator/Main.java
+++ src/day05/Calculator/Main.java
@@ -1,1 +1,22 @@
+import calculator.Calc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Calc kalkulacka = new Calc();
+
+        kalkulacka.scanInput();
+        kalkulacka.vypocitaj();
+        kalkulacka.printVysledok();
+
+        double poVypocte = kalkulacka.getVysledok();
+
+
+
+    }
+}
