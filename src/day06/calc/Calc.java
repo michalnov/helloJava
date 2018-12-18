42982835d044e0e9f9659162d5bd3b15d7528725
michalnov <32653270+michalnov@users.noreply.github.com>
2018-12-18 09:01:45 +0100

    Update Calc.java

--- src/day06/calc/Calc.java
+++ src/day06/calc/Calc.java
@@ -1,1 +1,58 @@
+package calc;
 
+import java.util.Scanner;
+
+public class Calc {
+
+    private double vstup1;
+    private double vstup2;
+    private String operacia;  // + - * /
+    private double vysledok;
+
+
+    public void scanInput(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("zadaj prve cislo: ");
+        this.vstup1 = scanner.nextDouble();
+
+        System.out.println("zadaj operaciu: ");
+        this.operacia = scanner.next();
+
+        System.out.println("zadaj druhe cislo: ");
+        this.vstup2 = scanner.nextDouble();
+    }
+
+    public void vypocitaj(){
+        //this.vysledok = vstup1 operator vstup2; // neda sa
+        switch (this.operacia){
+            case "+":{
+                this.vysledok = vstup1 + vstup2;
+                break;
+            }
+            case "-":{
+                this.vysledok = vstup1 - vstup2;
+                break;
+            }
+            case "*":{
+                this.vysledok = vstup1 * vstup2;
+                break;
+            }
+            case "/":{
+                this.vysledok = vstup1 / vstup2;
+                break;
+            }
+            default:{
+                System.out.println("Nespravny vstup");
+            }
+        }
+    }
+
+    public double getVysledok() {
+        return vysledok;
+    }
+
+    public void printVysledok(){
+        System.out.println("vysledok je: " + this.vysledok);
+    }
+}
