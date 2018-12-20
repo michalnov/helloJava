c729bc70dc7dfee200f2c95de1878e9b3fde8213
michalnov <32653270+michalnov@users.noreply.github.com>
2018-12-20 11:49:25 +0100

    Update Controller.java

--- src/day07/ticket/src/sample/Controller.java
+++ src/day07/ticket/src/sample/Controller.java
@@ -34,6 +34,43 @@ public class Controller {
         //3 v cykle vyskladat obsah pre output
         //4 zapisat obsah do outputu
         System.out.println("Reload");
+
+        output.setText("");
+
+        if (connection == null){
+            createConnection();
+        }
+
+
+        // int c = 8;
+        // c = c + 7;
+        // ekvivalent
+        // int c = 8;
+        // c += 7;
+
+        try {
+            String outputContent = "";
+            String query = "select * from tickets";
+            Statement statement = connection.prepareStatement(query);
+            ResultSet result = ((PreparedStatement) statement).executeQuery();
+            while (result.next()){
+                String id = result.getString("id");
+                String name = result.getString("name");
+                String description = result.getString("description");
+                String reserved = result.getString("reserved");
+
+                outputContent += "id: "+id + " name: " + name + " reserved n/y: " + reserved + "\n" + description + "\n\n";
+
+                System.out.println(name);
+            }
+            output.setText(outputContent);
+        } catch (SQLException e){
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 
     public void actionCreate(){
@@ -66,6 +103,7 @@ public class Controller {
             e.printStackTrace();
         }
 
+        actionReload();
         // insert into tickets(name, description, reservation) values (name, description, 'n');
     }
 
@@ -81,12 +119,40 @@ public class Controller {
         // pripravit delete v DB
         // vykonat delete
         System.out.println("Delete");
+
+        if (connection == null){
+            createConnection();
+        }
+
+        try {
+            String deleteIDvalue = deleteID.getText().trim();
+            if (deleteIDvalue.equals("")){
+                return;
+            }
+            String regex = "^[0-9]+$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(deleteIDvalue);
+
+            if (!matcher.matches()){
+                return;
+            }
+
+            String query = "delete from tickets where id = ?";
+            Statement statement = connection.prepareStatement(query);
+            ((PreparedStatement) statement).setInt(1,Integer.valueOf(deleteIDvalue));
+            ((PreparedStatement) statement).execute();
+        } catch (SQLException e){
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        actionReload();
     }
 
     public void createConnection(){
 
         final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-        final String DB_URL = "jdbc:mysql://localhost:3306/tickets?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        final String DB_URL = "jdbc:mysql://localhost:3306/tickets?useUnicode=true&characterEncoding=UTF-8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
         final String user = "root";
         final String password = "";
@@ -102,11 +168,6 @@ public class Controller {
         } catch (Exception e){
 
         }
-
-
-
-
-
     }
 
 
